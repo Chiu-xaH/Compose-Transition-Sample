720a9a62a16b98bbe143f3b95fa26e0b4c2b3513
Chiu-xaH <zsh0908@outlook.com>
2026-02-26 00:10:50 +0800

    修复导航多级闪烁

--- navigation/src/main/java/com/xah/navigation/controller/NavigationController.kt
+++ navigation/src/main/java/com/xah/navigation/controller/NavigationController.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.xah.common.LogUtil
 import com.xah.navigation.anim.EffectLevel
 import com.xah.navigation.anim.NavTransition
 import com.xah.navigation.model.ActionType
@@ -44,23 +43,37 @@ class NavigationController(
         destination: Destination,
         launchMode: LaunchMode = LaunchMode.STANDARD
     ) {
-        val from = _stack.last()
+        scope.launch {
+            val from = _stack.last()
 
-        when (launchMode) {
-            LaunchMode.STANDARD -> {
-                // 默认模式，每次都创建新的 Entry 并加入栈
-                val newEntry = StackEntry(
-                    id = UUID.randomUUID().toString(),
-                    destination = destination
-                )
-                _stack += newEntry
-            }
-            LaunchMode.SINGLE_TOP -> {
-                // 如果栈顶是目标 Activity，则复用栈顶实例
-                if (_stack.isNotEmpty() && _stack.last().destination == destination) {
-                    // 如果栈顶就是目标，保持栈顶不变
-                    return
-                } else {
+            when (launchMode) {
+                LaunchMode.STANDARD -> {
+                    // 默认模式，每次都创建新的 Entry 并加入栈
+                    val newEntry = StackEntry(
+                        id = UUID.randomUUID().toString(),
+                        destination = destination
+                    )
+                    _stack += newEntry
+                }
+                LaunchMode.SINGLE_TOP -> {
+                    // 如果栈顶是目标 Activity，则复用栈顶实例
+                    if (_stack.isNotEmpty() && _stack.last().destination == destination) {
+                        // 如果栈顶就是目标，保持栈顶不变
+                        return@launch
+                    } else {
+                        val newEntry = StackEntry(
+                            id = UUID.randomUUID().toString(),
+                            destination = destination
+                        )
+                        _stack += newEntry
+                    }
+                }
+                LaunchMode.SINGLE_TASK -> {
+                    // 如果栈中已经有该目标 Activity，则清除该 Activity 之上的所有 Activity，并复用它
+                    val existingIndex = _stack.indexOfFirst { it.destination == destination }
+                    if (existingIndex != -1) {
+                        _stack.subList(existingIndex + 1, _stack.size).clear() // 清除目标 Activity 之上的所有元素
+                    }
                     val newEntry = StackEntry(
                         id = UUID.randomUUID().toString(),
                         destination = destination
@@ -68,68 +81,72 @@ class NavigationController(
                     _stack += newEntry
                 }
             }
-            LaunchMode.SINGLE_TASK -> {
-                // 如果栈中已经有该目标 Activity，则清除该 Activity 之上的所有 Activity，并复用它
-                val existingIndex = _stack.indexOfFirst { it.destination == destination }
-                if (existingIndex != -1) {
-                    _stack.subList(existingIndex + 1, _stack.size).clear() // 清除目标 Activity 之上的所有元素
-                }
-                val newEntry = StackEntry(
-                    id = UUID.randomUUID().toString(),
-                    destination = destination
-                )
-                _stack += newEntry
+            // 动画未进行时归位，不影响打断动画
+            if(!isTransitioning) {
+                transitionProgress.snapTo(0f)
             }
+            // 添加过渡动画
+            navTransition = NavTransition(
+                type = ActionType.PUSH,
+                from = from,
+                to = _stack.last()
+            )
         }
 
-        // 添加过渡动画
-        navTransition = NavTransition(
-            type = ActionType.PUSH,
-            from = from,
-            to = _stack.last()
-        )
     }
 
     fun pop() {
-        if (_stack.size <= 1) return
+        scope.launch {
+            if (_stack.size <= 1) return@launch
 
-        val from = _stack.last()
-        val to = _stack[_stack.lastIndex - 1]
+            val from = _stack.last()
+            val to = _stack[_stack.lastIndex - 1]
+            // 动画未进行时归位，不影响打断动画
+            if(!isTransitioning) {
+                transitionProgress.snapTo(1f)
+            }
+            navTransition = NavTransition(
+                type = ActionType.POP,
+                from = from,
+                to = to,
+            )
+        }
 
-        navTransition = NavTransition(
-            type = ActionType.POP,
-            from = from,
-            to = to,
-        )
     }
 
     /**
      * 回到startDestination（栈中有则复用，无则清空栈再push）
      */
     fun home() {
-        val from = _stack.last()
-        // 从栈底（索引0）开始寻找 startDestination
-        var found = false
-        for (i in _stack.indices) {
-            if (_stack[i].destination == startDestination) {
-                // 如果找到了startDestination，清空栈并只保留 startDestination
-                _stack.subList(i + 1, _stack.size).clear()
-                found = true
-                break
+        scope.launch {
+            val from = _stack.last()
+            // 从栈底（索引0）开始寻找 startDestination
+            var found = false
+            for (i in _stack.indices) {
+                if (_stack[i].destination == startDestination) {
+                    // 如果找到了startDestination，清空栈并只保留 startDestination
+                    _stack.subList(i + 1, _stack.size).clear()
+                    found = true
+                    break
+                }
             }
-        }
 
-        if (!found) {
-            // 栈中没有 startDestination，清空栈并 push startDestination
-            _stack.clear()
-            addHome()
+            if (!found) {
+                // 栈中没有 startDestination，清空栈并 push startDestination
+                _stack.clear()
+                addHome()
+            }
+            // 动画未进行时归位，不影响打断动画
+            if(!isTransitioning) {
+                transitionProgress.snapTo(1f)
+            }
+            // 添加过渡动画
+            navTransition = NavTransition(
+                type = ActionType.POP,
+                from = from,
+                to = _stack.last()
+            )
         }
-        // 添加过渡动画
-        navTransition = NavTransition(
-            type = ActionType.POP,
-            from = from,
-            to = _stack.last()
-        )
     }
 
     fun animate(
@@ -145,13 +162,6 @@ class NavigationController(
         animationSpec: AnimationSpec<Float> = defaultSpec
     ) {
         navTransition ?: return
-        // 动画未进行时归位，不影响打断动画
-        if(!isTransitioning) {
-            transitionProgress.snapTo(when (navTransition!!.type) {
-                ActionType.PUSH -> 0f
-                ActionType.POP -> 1f
-            })
-        }
 
         val target = when (navTransition!!.type) {
             ActionType.PUSH -> 1f
